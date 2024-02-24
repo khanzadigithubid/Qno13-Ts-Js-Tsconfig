02798ed79220f30415de3494e763e04546cef105
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:37:27 +0500

    Qno13 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno13 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno13.js
@@ -0,0 +1,7 @@
+"use strict";
+/*Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a
+ car, and make a list that stores several examples. Use your list to print a series of
+ statements about these items, such as “I would like to own a Honda motorcycle.”
+ Define an array called 'favoriteTransportation' with car examples*/
+let vechiles = ["Tesla Model S", "BMW M5", "Audi R8", "Ford Mustang"];
+vechiles.map((items) => console.log(`I would like to own a ${items}`));
---
+++ Qno13.ts
@@ -0,0 +1,8 @@
+/*Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a
+ car, and make a list that stores several examples. Use your list to print a series of 
+ statements about these items, such as “I would like to own a Honda motorcycle.”
+ Define an array called 'favoriteTransportation' with car examples*/
+
+let vechiles: string[] = ["Tesla Model S", "BMW M5", "Audi R8", "Ford Mustang"]
+vechiles.map((items) => console.log(`I would like to own a ${items}`));
+
